72438b7767eec20f70c238211be8a826519cdfe4
Filipe Dias <dias.filipe2000gmail.com>
2024-06-09 17:01:01 +0100

    Created Ironhack Web App

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Define the function that will be called when the button is clicked
-function onClick() {
-    alert("This is an awesome page");
-}
-
-// Add an event listener to the button to call the onClick function when it's clicked
-document.getElementById("alertButton").addEventListener("click", onClick);
+// script.js
+$(document).ready(function() {
+    $('#button').click(function() {
+      alert('This app is awesome!');
+    });
+  });
+  
